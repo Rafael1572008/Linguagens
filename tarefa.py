0c5af684c741fc5969c16d3a66b5feb128dc4eb2
Rafael P <127984148+Rafael1572008@users.noreply.github.com>
2024-03-01 11:43:42 -0300

    Add files via upload

---
+++ tarefa.py
@@ -0,0 +1,27 @@
+class Veiculo:
+    def __init__(self, marca, ano, preco):
+        self.marca = marca
+        self.ano = ano
+        self.preco = preco
+
+    def info(self):
+        return f"Marca: {self.marca}\nAno: {self.ano}\nPreço: {self.preco}"    
+
+class Carro(Veiculo):
+    def __init__(self, marca, ano, preco, modelo, potencia):
+        super().__init__(marca, ano, preco)
+        self.modelo = modelo
+        self.potecia = potencia
+
+    def info(self):
+        return super().info() + f"\n Modelo: {self.modelo}\nPotencia: {self.potecia}"
+
+class Moto(Veiculo):
+    def __init__(self, marca, ano, preco, cilindrada):
+        super().__init__(marca, ano, preco)
+        self.cilindrada = cilindrada
+
+    def info(self):
+        return super.info() + f"Cilindrada: {self.cilindrada}"
+    
+itapava = Moto("itaipava", 2018, )
